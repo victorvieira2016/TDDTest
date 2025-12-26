c12d33d6acf3d09e43727775e9292166ba77b3b1
Victor Vieira <victor.a.vieira2014@gmail.com>
2025-12-26 00:48:34 -0400

    Create test for Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+public final class Calculadora {
+    private Calculadora () {};
 
-public class Calculadora {
+    public static double soma(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public static double subtrai(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+
+    public static double multiplica(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+
+    public static double dividir(double valor1, double valor2) {
+        return valor1 / valor2;
+    }
 }
--- test/src/CalculadoraTest.java
+++ test/src/CalculadoraTest.java
@@ -1,4 +1,72 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CalculadoraTest {
+
+    @Test
+    public void somar() {
+        // arrange
+        double valor1 = 2;
+        double valor2 = 2;
+
+        // act
+        double resultado = Calculadora.soma(valor1, valor2);
+
+        // assert
+        Assertions.assertEquals(4.0, resultado);
+    }
+
+    @Test
+    public void subtrair() {
+        // act
+        double resultado = Calculadora.subtrai(10,5);
+
+        // assert
+        Assertions.assertEquals(5.0, resultado);
+    }
+
+    @Test
+    public void multiplicar() {
+        // act
+        double resultado = Calculadora.multiplica(2,3);
+
+        // assert
+        Assertions.assertEquals(6, resultado);
+    }
+
+    @Test
+    public void divisaoPorZeroPositiva() {
+        // act
+        double resultado = Calculadora.dividir(10,0);
+
+        // assert
+        Assertions.assertEquals(Double.POSITIVE_INFINITY, resultado);
+    }
+
+    @Test
+    public void divisaoPorZeroNegativa() {
+        // act
+        double resultado = Calculadora.dividir(-10,0);
+
+        // assert
+        Assertions.assertEquals(Double.NEGATIVE_INFINITY, resultado);
+    }
+
+    @Test
+    public void divisao() {
+        // act
+        double resultado = Calculadora.dividir(10,2);
+
+        // assert
+        Assertions.assertEquals(5.0, resultado);
+    }
+
+    @Test
+    public void divisaoZeroPorZero() {
+        // act
+        double resultado = Calculadora.dividir(0,0);
+
+        // assert
+        Assertions.assertEquals(Double.NaN, resultado);
+    }
 }
